568bc698c68cd055608b38b7efeb500b73bb7f35
LeoMasago <leo.masago@etec.sp.gov.br>
2024-04-23 11:13:25 -0300

    Testando tela de início

---
+++ main.js
@@ -0,0 +1,11 @@
+function idade(){
+    var idade = parseFloat(document.getElementById("idade").value);
+
+    if(idade < 18){
+        alert("Não pode!")
+    }
+    else{
+        // direcionando para a página home
+        window.location.href = "home.html"
+    }
+}
